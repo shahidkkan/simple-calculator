a31b402a6cb20d4ca59a779c489248d33becdcf6
Shahid khan <161056935+shahidkkan@users.noreply.github.com>
2024-03-16 05:00:39 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstnumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+
+  // conditional statement
+
+  if (answer. operator === "addition"){
+
+    console.log(answer. firstnumber + answer. secondnumber)
+
+  }else if (answer. operator === "subtraction"){
+     console.log(answer. firstnumber - answer. secondnumber)
+
+  }else if (answer. operator === "multiplication"){
+     console.log(answer. firstnumber * answer. secondnumber)
+
+  }else if (answer. operator === "division"){
+     console.log(answer. firstnumber / answer. secondnumber)
+
+  }else {
+     console.log("please select valid operator")
+  }
